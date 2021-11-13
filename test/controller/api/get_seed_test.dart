ccfafb594c9484151efe9711a52917d318cb8b20
Andrae J Henry <andrae.henry@icloud.com>
2021-11-12 23:04:18 -0500

    Create networking test

---
+++ test/controller/api/get_seed_test.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:qr_code_test/controller/api/get_seed.dart';
+import 'package:qr_code_test/model/seed_model.dart';
+
+void main() {
+  test('get seed ...', () async {
+    final seedAPI = Networking();
+    seedAPI.client = MockClient((request) async {
+      return Response(
+          json.encode(
+              {'seed': 'abcdefghijkl', 'expiresAT': "2021-11-12T21:45:30"}),
+          200);
+    });
+
+    // Seed? seed = await seedAPI.getSeed()
+  });
+}
